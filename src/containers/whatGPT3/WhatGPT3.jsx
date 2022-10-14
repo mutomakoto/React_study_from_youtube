d1803a0b93cd0362f7eba34df030f6985676d05e
mutomakoto <aaakbaru@gmail.com>
2022-10-14 15:59:24 +0900

    whatgpt3の途中まで

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,12 +4,12 @@ import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__wahtgpt3 section__margin" id="whgpt3">
+    <div className="gpt3__whatgpt3 section__margin" id="whgpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">
+        <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
         <p>Explore The Library</p>
